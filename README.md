76d6b66620cae367437da4ce2c884ff06d79073a
Joseph Mignone <jmig3066@gmail.com>
2023-10-04 19:35:25 -0400

    Add support for run conditions

--- README.md
+++ README.md
@@ -24,8 +24,6 @@ NOTE: This applet is not currently in HACS' default repository, so it needs to b
 
 ### Manual install (no updates)
 - Download this repository, and copy the "schedule" folder to "appdaemon/apps".
-
-
 - Add your configuration to `apps.yaml` by modifying one of the examples below to fit your needs. Once you add the configuration, the module will be enabled.
 
 
@@ -84,8 +82,32 @@ In the above example, you might have noticed that the `minute` variable was miss
 | `second`   | <i> True  </i> | `0` | A second value (0 to 59)
 | `meridiem` | <i> False </i> |     | AM/PM
 
+## Condition support for schedules
+#### <i>This feature was introduced with version 1.0.2.</i>
+Run conditions can be used to determine if your schedule should work or not. For example, if you have an irrigation system schedule, but there's rain in the weather forecast, you can use a condition to only run the irrigation system if the forecast is sunny.
 
+### Here's an example of the condition parameter
+```yaml
+# Irrigation system
+my_schedules:
+  module: schedule
+  class: Schedule
+  schedules:
+    water_the_plants_on_one_condition:
+      on_at: '8:00:00'
+      off_at: '8:15:00'
+      target_entity: switch.irrigator_switch
+      conditions:
+        - sensor: weather.kfmh_daynight
+          state: "sunny" # Only run if it's sunny out
+```
+
+
+## Schedules: extra parameters
+| Parameter name | Optional | Default value | Description |
+| --- | --- | --- | --- |
+| `[schedule].react_immediately_when_conditions_change`   | <i> True </i> | False | If set to true, as soon as a condition changes (eg. a switch is toggled), instead of waiting until the next cycle, immediately turn on/off the controlled entity (depending on whether it is supposed to be on or not)
+| `[schedule].change_state_on_schedule_load`              | <i> True </i> |       | When the schedule is initialized (eg. after a settings change or at bootup), automatically set the state of the controlled entity (for example, if the current time is between the times specified in the `on_at` and `off_at` parameters, turn the entity on <i>now</i> instead of waiting for the next cycle).
 
 ## <b>TODO: Plans for the future</b>
-- Add the ability to specify run conditions for schedules
 - Allow for specifying a run duration instead of an end time
--- apps/schedule/schedule.py
+++ apps/schedule/schedule.py
@@ -1,79 +1,158 @@
 # Schedule
 # Written by Joseph Mignone
 # 10/4/2023
-# Version 1.0.1
+# Version 1.1.0
 
 import appdaemon.plugins.hass.hassapi as hass
 from datetime import datetime
 from datetime import time
 
-# Variable considerations:
-#
-# react_immediately_when_condition_changes: Bool
-#   If the condition changes, react immediately. For example, if the condition evaluates to 'True', and we're within the time that the entity should be on, turn the entity on immediately instead of waiting for the next cycle. Defaults to False.
+class Conditions:
+    def __init__(self, hass, conditions, callback, schedule_entity):
+        self.hass = hass
+        self.conditions = conditions
+        self.enabled = (conditions is not None and len(conditions) > 0)
+        self.callback = callback
+        self.schedule_entity = schedule_entity
+
+        if self.enabled:
+            # Establish state listeners for each sensor
+            for condition in self.conditions:
+                sensor = condition["sensor"]
+                self.hass.listen_state(self.sensor_state_changed_callback, sensor)
+
+    def check(self):
+        if self.enabled == True:
+            for condition in self.conditions:
+                sensor = condition["sensor"]
+                state = condition["state"]
+                if self.hass.get_state(sensor) != state:
+                    return False  # Something didn't pass the sniff test...
+            return True  # All checks pass!
+        else:
+            return True  # The checks are disabled, or we don't have any conditions to meet. Give the A-OK.
+
+    def sensor_state_changed_callback(self, entity, attribute, old, new, kwargs):
+        self.schedule_entity.print_log(f"Sensor state changed for entity '{entity}': attribute '{attribute}' value changed from {old} changed to {new}")
+        self.schedule_entity.print_log("Running condition check...")
+        check_result = self.check()
+        if check_result:
+            self.schedule_entity.print_log("Conditions checked out okay")
+        else:
+            self.schedule_entity.print_log("Condition check failed")
+        self.callback(check_result)
+
+
+class ScheduleEntity():
+    def __init__(self, hass, schedule_name, schedule_info):
+        self.hass = hass
+        self.schedule_name = schedule_name
+        self.schedule_info = schedule_info
+
+        self.on_time = self.hass.parse_time(convert_time(self.schedule_info["on_at"]))
+        self.off_time = self.hass.parse_time(convert_time(self.schedule_info["off_at"]))
+        self.target_entity = self.schedule_info["target_entity"]
+        self.change_state_on_schedule_load = self.schedule_info.get("change_state_on_schedule_load", False)  # When the schedule is loaded, immediately determine if entity should be on or off, and set its state accordingly
+        
+        # Condition handling
+        self.react_immediately_when_conditions_change = self.schedule_info.get("react_immediately_when_conditions_change", False)
+        conditions = self.schedule_info.get("conditions", None)
+        self.conditions_manager = Conditions(self.hass, conditions, self.condition_check_callback, self)
+
+        # Create scheduled tasks
+        self.hass.run_daily(self.turn_on_with_conditions, self.on_time)
+        self.hass.run_daily(self.turn_off_with_conditions, self.off_time)
+        self.hass.log(f"Established schedule \"{self.schedule_name}\" (on at {self.on_time}, off at {self.off_time})")
+
+        if self.change_state_on_schedule_load:
+            now = datetime.now().time()
+            if time_is_within_range(now, self.on_time, self.off_time):
+                self.turn_on()
+            else:
+                self.turn_off()
+
+
+    def condition_check_callback(self, check_result):
+        if check_result:
+            if self.react_immediately_when_conditions_change:
+                now = datetime.now().time()
+                if time_is_within_range(now, self.on_time, self.off_time):
+                    self.turn_on()
+        else:
+            if self.react_immediately_when_conditions_change:
+                self.turn_off()
+    
+    def turn_on_with_conditions(self, kwargs):
+        if self.conditions_manager.check():
+            self.turn_on()
+        else:
+            self.print_log(f"Not turning entity \"{self.target_entity}\" on: conditions not met")
+
+    def turn_off_with_conditions(self, kwargs):
+        if self.conditions_manager.check():
+            self.turn_off()
+        else:
+            self.print_log(f"Not turning entity \"{self.target_entity}\" off: conditions not met")
+
+
+    def turn_on(self):
+        self._turn_on({"entity": self.target_entity})
+    
+    def turn_off(self):
+        self._turn_off({"entity": self.target_entity})
+
+
+    def _turn_on(self, kwargs):
+        entity = kwargs["entity"]
+        self.print_log(f"Turning {entity} on")
+        self.hass.call_service("homeassistant/turn_on", entity_id=entity)
+
+    def _turn_off(self, kwargs):
+        entity = kwargs["entity"]
+        self.print_log(f"Turning {entity} off")
+        self.hass.call_service("homeassistant/turn_off", entity_id=entity)
+
+    def print_log(self, log):
+        self.hass.log(f"Schedule \"{self.schedule_name}\": {log}")
+
 
 class Schedule(hass.Hass):
     def initialize(self):
         self.schedules = self.args.get("schedules", {})
-        # self.log(self.schedules)
+        self.schedule_objects = []
+        
         for schedule_name, schedule_info in self.schedules.items():
-            on_time = self.parse_time(self.convert_time(schedule_info["on_at"]))
-            off_time = self.parse_time(self.convert_time(schedule_info["off_at"]))
+            self.schedule_objects.append(ScheduleEntity(self, schedule_name, schedule_info))
 
-            target_entity = schedule_info["target_entity"]
 
-            now = datetime.now().time()
-            self.log(now)
-            if time_is_within_range(now, on_time, off_time):
-                self.turn_on({"entity": target_entity})
-            else:
-                self.turn_off({"entity": target_entity})
-            
-            self.run_daily(self.turn_on, on_time, entity=target_entity)
-            self.run_daily(self.turn_off, off_time, entity=target_entity)
-            self.log(f"Established schedule \"{schedule_name}\" (on at {on_time}, off at {off_time})")
 
-    def turn_on(self, kwargs):
-        entity = kwargs["entity"]
-        self.log(f"Turning {entity} on")
-        self.call_service("homeassistant/turn_on", entity_id=entity)
+def convert_time(time):
+    error_message_function_name = "convert_time"
+    acceptable_value_error_message = "Acceptable values are AM and PM."
+    parameter_must_be_defined_error_message = lambda x : f"parameter '{x}' must be defined!"
 
-    def turn_off(self, kwargs):
-        entity = kwargs["entity"]
-        self.log(f"Turning {entity} off")
-        self.call_service("homeassistant/turn_off", entity_id=entity)
-
-    def convert_time(self, on_time):
-        error_message_function_name = "convert_time"
-        acceptable_value_error_message = "Acceptable values are AM and PM."
-        parameter_must_be_defined_error_message = lambda x : f"parameter '{x}' must be defined!"
-
-        if isinstance(on_time, str):
-            return on_time  # Assume that it is already in 24-hour format
-        else: # Convert 12hr to 24hr
-            hour = on_time.get('hour')
-            minute = on_time.get('minute', 0) # These can be default to 0
-            second = on_time.get('second', 0) # These can be default to 0
-            meridiem = on_time.get('meridiem')
-
-            if hour is None:
-                raise ValueError(f"{error_message_function_name}: {parameter_must_be_defined_error_message('hour')}")
-
-            if meridiem is None:
-                raise ValueError(f"{error_message_function_name}: {parameter_must_be_defined_error_message('meridiem')} {acceptable_value_error_message}")
-
-            if meridiem.lower() == 'pm':
-                # convert
-                if hour != 12:
-                    hour += 12
-            elif meridiem.lower() == 'am':
-                # convert
-                if hour == 12:
-                    hour = 0
-            else:
-                raise ValueError(f"{error_message_function_name}: 'meridiem': bad value. {acceptable_value_error_message} Got '{meridiem}'")
-
-            return f"{hour:02d}:{minute:02d}:{second:02d}"
+    if isinstance(time, str):
+        return time  # Assume that it is already in 24-hour format
+    else: # Convert 12hr to 24hr
+        hour = time.get('hour')
+        minute = time.get('minute', 0) # These can be default to 0
+        second = time.get('second', 0) # These can be default to 0
+        meridiem = time.get('meridiem')
+        if hour is None:
+            raise ValueError(f"{error_message_function_name}: {parameter_must_be_defined_error_message('hour')}")
+        if meridiem is None:
+            raise ValueError(f"{error_message_function_name}: {parameter_must_be_defined_error_message('meridiem')} {acceptable_value_error_message}")
+        if meridiem.lower() == 'pm':
+            # convert
+            if hour != 12:
+                hour += 12
+        elif meridiem.lower() == 'am':
+            # convert
+            if hour == 12:
+                hour = 0
+        else:
+            raise ValueError(f"{error_message_function_name}: 'meridiem': bad value. {acceptable_value_error_message} Got '{meridiem}'")
+        return f"{hour:02d}:{minute:02d}:{second:02d}"
 
 
 def time_is_within_range(time: time, start: time, end: time) -> bool:
